c54676fd86bedb1994168466a9a0814a51bd33a4
Edimar <edimarjr51@gmail.com>
2026-06-03 15:42:29 -0300

    feat: add global handler for IllegalStateException

--- biblioteca/src/main/java/com/NovaStack/biblioteca/infra/exception/GlobalException.java
+++ biblioteca/src/main/java/com/NovaStack/biblioteca/infra/exception/GlobalException.java
@@ -10,8 +10,19 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalException {
 
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ResponseError> catchIllegalStateException(IllegalStateException ex){
+        System.out.println(ex.getMessage());
+        ResponseError response = new ResponseError(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ResponseError> catchGlobalException(Exception ex){
+        System.out.println(ex.getMessage());
         ResponseError response = new ResponseError(
                 "An unexpected error occurred",
                 HttpStatus.INTERNAL_SERVER_ERROR,
