9ec5eaaf9a8f94ecdf26fa0852521402279b89df
Edimar <edimarjr51@gmail.com>
2026-06-08 19:57:56 -0300

    fix: allow PATCH requests in CORS configuration

--- biblioteca/src/main/java/com/NovaStack/biblioteca/infra/configuration/CorsConfiguration.java
+++ biblioteca/src/main/java/com/NovaStack/biblioteca/infra/configuration/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5501", "http://localhost:5501")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS");
     }
 
 }
