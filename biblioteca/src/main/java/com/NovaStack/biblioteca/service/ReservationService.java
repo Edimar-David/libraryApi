23894f390ea21df9aef6c80fdc570a21e57618d1
Edimar <edimarjr51@gmail.com>
2026-06-14 15:33:02 -0300

    feat: add ReservationService

---
+++ biblioteca/src/main/java/com/NovaStack/biblioteca/service/ReservationService.java
@@ -0,0 +1,11 @@
+package com.NovaStack.biblioteca.service;
+
+import com.NovaStack.biblioteca.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+    @Autowired
+    ReservationRepository reservationRepository;
+}
